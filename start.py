fe588c56cb72c1a0682ca93878a13ac95eee734f
masterpoku <wafitakputusasa@gmail.com>
2024-11-16 21:11:39 +0700

    makan

--- start.py
+++ start.py
@@ -17,6 +17,9 @@ url = "https://287a-36-71-164-132.ngrok-free.app/slt/api.php"
 data = [None]  # Menggunakan list untuk menyimpan data
 should_read_input = [False]  # Menggunakan list untuk menyimpan flag pembacaan input
 
+# Status koneksi
+connection_status = False
+
 # Fungsi untuk memeriksa koneksi internet
 def is_connected():
     try:
@@ -50,18 +53,26 @@ def fetch_data(url, data, should_read_input):
 
 # Menjalankan kedua fungsi dalam thread terpisah
 def main():
-    # Memeriksa koneksi internet
-    if is_connected():
-        print("Koneksi internet terdeteksi, menjalankan fungsi read_device_events...")
-        # Memulai thread untuk mengambil data dari API dan membaca perangkat input
-        threading.Thread(target=fetch_data, args=(url, data, should_read_input), daemon=True).start()
-        threading.Thread(target=read_device_events, args=(dev, should_read_input), daemon=True).start()
-    else:
-        print("Tidak ada koneksi internet, menggunakan valid_rfid...")
-        while True:
-            # Jika tidak ada koneksi, gunakan valid_rfid
+    global connection_status
+    
+    while True:
+        # Memeriksa koneksi internet
+        if is_connected():
+            if not connection_status:  # Jika sebelumnya tidak terhubung, dan sekarang terhubung
+                print("Koneksi internet terdeteksi, menjalankan fungsi read_device_events...")
+                # Memulai thread untuk membaca perangkat input
+                threading.Thread(target=read_device_events, args=(dev, should_read_input), daemon=True).start()
+                connection_status = True  # Update status koneksi
+            # Memulai thread untuk mengambil data dari API
+            threading.Thread(target=fetch_data, args=(url, data, should_read_input), daemon=True).start()
+        else:
+            if connection_status:  # Jika sebelumnya terhubung, tapi sekarang terputus
+                print("Koneksi internet terputus, beralih ke valid_rfid...")
+                connection_status = False  # Update status koneksi
             print(f"RFID yang valid: {valid_rfid}")
-            time.sleep(1)
+        
+        # Tunggu sebelum memeriksa koneksi lagi
+        time.sleep(5)
 
 # Menjalankan program utama
 if __name__ == "__main__":
